9641939c92ca97cd3ad19741870b224a80cfee87
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2017-11-11 16:47:58 +0000

    Adicionadas estruturas

--- structs.h
+++ structs.h
@@ -3,3 +3,30 @@ struct users {
   char password[20];
   struct users *next;
 };
+struct usersActive {
+  char username[20];
+  int pid;
+};
+
+struct usersPlaying {
+  char username[20];
+  int pontuation;
+  int position [1][1];
+  int miniBombs = 3;
+  int bigBombs = 2;
+  int timeToPlay;
+};
+
+struct board {
+  int block[50][50];
+  int exit[1][1];
+  int object [50][50];
+  int walls[50][50];
+  int miniBomb[50][50];
+  int bigBomb[50][50];
+  int enemies [50][50];
+  int colector [50][50];
+  int extra [50][50];
+  int time [1][1][1];
+  usersPlaying players;
+};
